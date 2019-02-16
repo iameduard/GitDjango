6dd75364c7b92165bfc9f2c76b06a921971cf073
iameduard <iameduard@hotmail.com>
2019-02-16 19:09:32 -0400

    forth commit

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse,resolve
-from .views import home, broad_topics
+from .views import home, board_topics
 from .models import Board
 
 # Create your tests here.
@@ -8,8 +8,27 @@ class HomeTests(TestCase):
 	def test_home_view_status_code(self):
 		url=reverse('home')
 		response = self.client.get(url)
-		self.assertEqual(response.status_code,200)
+		self.assertEquals(response.status_code,200)
 
 	def test_home_url_resolvers_home_view(self):
 		view=resolve('/')
-		self.assertEqual(view.func,home)
+		self.assertEquals(view.func,home)
+
+class BoardTopicsTest(TestCase):
+	def setUp(self):
+		Board.objects.create(name='Django',description='Django board.')
+
+	def test_board_topics_view_success_status_code(self):
+		url=reverse('board_topics',kwargs={'pk':1})
+		response=self.client.get(url)
+		self.assertEquals(response.status_code,200)
+
+	def test_board_topics_view_not_found_status_code(self):
+		url=reverse('board_topics',kwargs={'pk':99})
+		response=self.client.get(url)
+		self.assertEquals(response.status_code,404)
+
+	def test_board_topics_url_resolver_board_topics_view(self):
+		view=resolve('/boards/1/')
+		self.assertEquals(view.func,board_topics)
+
--- boards/views.py
+++ boards/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Board
+from django.shortcuts 	import render, get_object_or_404
+from django.http 		import HttpResponse, Http404
+from .models 			import Board
 # Create your views here.
 def home(request):
 	boards=Board.objects.all()
 	return render(request,'home.html',{'boards':boards})
+	pass
 
 def board_topics(request,pk):
-	board=Board.objects.get(pk=pk)
+	board = get_object_or_404(Board,pk=pk)
 	return render(request,'topics.html',{'board':board})
 
